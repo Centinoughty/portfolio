ac30a673cbb9ac5b31cedebd0b49ea26b0dba393
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-05-30 05:58:22 +0530

    Home page completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import TypingEffect from "@/util/TypingEffect";
 import { typingData } from "@/data/content";
 import Skill from "@/components/Skills/Skill";
 import Button from "@/util/Button";
+import Framework from "@/components/Frameworks/Framework";
 
 export default function Home() {
   return (
@@ -15,13 +16,15 @@ export default function Home() {
           className="rounded-full justify-self-center h-[250px] w-[250px]"
         />
         <h1 className="text-5xl sm:text-6xl md:text-8xl font-diph font-bold">
-          Hi. I'm Nadem
+          Hi. I'm Nadeem.
         </h1>
         <p className="text-3xl md:text-4xl font-mont">
           A <TypingEffect data={typingData} />
         </p>
+        <p className="text-black font-diph text-xl">"The only way to do great work is to love what you do." - Steve Jobs</p>
       </main>
       <Skill />
+      <Framework />
       <Footer />
     </>
   );
